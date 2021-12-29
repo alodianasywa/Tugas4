200d8cd7cebf490b08c93ae086c54ad826957dc4
alodianasywa <alodnasywa02@gmail.com>
2021-12-29 21:33:25 +0700

    First Commit

---
+++ app/src/main/java/com/example/myfirebasecrud/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myfirebasecrud;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btCreateDB;
+    private Button btViewDB;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
